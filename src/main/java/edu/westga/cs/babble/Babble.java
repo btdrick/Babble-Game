f0ebb2c2c600929a97868fe97e9c1f6201d7304a
Brandon Drick <bd00051@my.westga.edu>
2021-08-31 16:25:27 -0500

    Completed initial front-end GUI development and connected to Babble
    GUI contains:
    -ListView for displaying TileRack tiles
    -ListView for displaying selected tiles
    -"Reset" button
    -"Play Word" button
    -TextField for displaying score

--- src/main/java/edu/westga/cs/babble/Babble.java
+++ src/main/java/edu/westga/cs/babble/Babble.java
@@ -17,20 +17,21 @@ import javafx.stage.Stage;
 public class Babble extends Application {
 
 	private static final String GUI_RESOURCE = "edu/westga/cs/babble/views/BabbleGui.fxml";
-    @Override
+    
+	@Override
     public void start(Stage primaryStage) throws Exception {
-//        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-//        URL resource = classLoader.getResource(Babble.GUI_RESOURCE);
-//        FXMLLoader loader = new FXMLLoader(resource);
-//        Parent root = (Parent) loader.load();
-//        Scene scene = new Scene(root);
-//        primaryStage.setScene(scene);
-//        primaryStage.setTitle("Babble");
-//        primaryStage.show();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = classLoader.getResource(Babble.GUI_RESOURCE);
+        FXMLLoader loader = new FXMLLoader(resource);
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Babble");
+        primaryStage.show();
     }
 
     /**
-     * Start point for the appliction.
+     * Start point for the application.
      * 
      * @param args not used
      */
